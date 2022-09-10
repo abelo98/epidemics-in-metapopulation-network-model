34e183efc49ea55b9c65b7bd4396b0373b88ad83
Abel <abelo08cruz@gmail.com>
2022-09-10 16:14:33 -0400

    setting up the main method

--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ from mmodel.calc_params import Initialized
 from mmodel.data_reader import Reader
 from mmodel.data_cleaner import Cleaner
 
+data_conf_path = "/media/abel/TERA/School/5to/tesis stuff/cv19_conf_mun.xlsx"
+data_dead_path = "/media/abel/TERA/School/5to/tesis stuff/cv19_fall_mun.xlsx"
+
+
 ydata =  Initialized()
 
-reader = Cleaner()
+cleaner = Cleaner()
 reader = Reader()
--- src/mmodel/data_cleaner.py
+++ src/mmodel/data_cleaner.py
@@ -4,7 +4,8 @@ from pandas import DataFrame
 class Cleaner:
     def __init__(self) -> None:
         pass
-
+    
+    @staticmethod
     def select_rows(df: DataFrame,muncps):
         return dict(zip(muncps,[df.loc(df[m]) for m in muncps]))
          
