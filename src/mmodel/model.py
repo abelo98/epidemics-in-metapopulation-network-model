bfda16ec5c58822235157c578bcf2c6b9760320d
Abel <abelo08cruz@gmail.com>
2022-09-18 20:56:31 -0400

    modifies my model

--- src/mmodel/model.py
+++ src/mmodel/model.py
@@ -1,18 +1,9 @@
-from scipy import integrate
-
 
 class SIR:
-    def __init__(self, initial_v: dict) -> None:
-        self.S0 = initial_v['S']
-        self.I0 = initial_v['I']
-        self.R0 = initial_v['R']
-        self.N = initial_v['N']
-
     @staticmethod
     def sir_ecuations(y, x, beta, gamma):
-        S = -beta * y[0] * y[1] / (sum(y))
-        R = gamma * y[1]
-        I = -(S + R)
-        return S, I, R
-
-
+        S, I, R = y
+        dSdt = -beta * S * I / (sum(y))
+        dRdt = gamma * I
+        dIdt = -(S + R)
+        return dSdt, dIdt, dRdt
