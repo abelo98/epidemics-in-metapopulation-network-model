66916559094dee10c0fcd495bc0e55c2c1375068
Abel <abelo08cruz@gmail.com>
2022-09-21 18:16:46 -0400

    refactorizes the code

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+MUNCPS = ["Playa",
+              "Plaza de la Revolución",
+              "Centro Habana",
+              "Habana Vieja",
+              "Regla",
+              "Habana del Este",
+              "Guanabacoa",
+              "San Miguel del Padrón",
+              "Diez de Octubre",
+              "Cerro",
+              "Marianao",
+              "La Lisa",
+              "Boyeros",
+              "Arroyo Naranjo",
+              "Cotorro"]
+
+START_INFECTED = 15 #constant to get the infected from the day 15
---
+++ src/mmodel/json_manager/json_processor.py
@@ -0,0 +1,18 @@
+
+import json
+
+
+def read_json(current_paramas_json):
+    with open(current_paramas_json, 'r') as f:
+        json_parsed = json.load(f)
+        f.close()
+        return json_parsed
+
+
+def save_file_as_json(path, file: list):
+    with open(path, 'w') as f:
+        serialized_json = json.dumps(file, indent=4)
+        f.write(serialized_json)
+        f.close()
+
+
