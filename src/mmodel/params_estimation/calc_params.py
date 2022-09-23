f4e56537808635ffc30fe332b29f93e64a351420
Abel <abelo08cruz@gmail.com>
2022-09-23 17:05:46 -0400

    brings changes from params_estimation_feature

--- src/mmodel/params_estimation/calc_params.py
+++ src/mmodel/params_estimation/calc_params.py
@@ -138,14 +138,21 @@ from ..constants import BETA, GAMMA, START_INFECTED
 #     return integrate.odeint(SIR.sir_ecuations, i_values, x, args=(beta, gamma))[:, 1]
 
 
-def __get_params__(params, munc, popt):
+def estimate_params(ydata: np.array, time: np.array, params: list, initial_v: dict, munc):
+    global i_values
+    i_values = tuple(initial_v.values())
+
+    popt, _ = optimize.curve_fit(fit_odeint, time, ydata, p0=[
+                                 0.17, 0.082], bounds=(0, 1), maxfev=5000)
+
     params_estimated = {}
-    print(munc)
-    print("params: ")
-    print("")
     for i, p in enumerate(popt):
+        print(munc)
+        print("params: ")
+        print("")
         print(f'{params[i]}: {p}')
         params_estimated[params[i]] = p
+
     return params_estimated
 
 
@@ -153,39 +160,21 @@ def fit_odeint(x, beta, gamma):
     return integrate.odeint(SIR.sir_ecuations, i_values, x, args=(beta, gamma))[:, 1]
 
 
-def estimate_params(ydata: np.array, time: np.array, params: list, initial_v: dict, munc):
-    global i_values
-    i_values = tuple(initial_v.values())
-
-    popt, _ = optimize.curve_fit(fit_odeint, time, ydata, p0=[
-        BETA, GAMMA],bounds=(0,1), maxfev=5000)
-
-    return __get_params__(params, munc, popt)
-
-
 def get_initial_values_SIR(json_file):
     S0 = json_file["y"]["S"]
     I0 = json_file["y"]["I"]
     R0 = json_file["y"]["R"]
-    N = json_file["y"]["N"]
 
-    return {"S": S0, "I": I0, "R": R0, "N": N}
-
-
-def set_initial_values(model, infected):
-    initial_v = get_initial_values_SIR(model)
-    infected_by_munc = infected[model["label"]][START_INFECTED:]
-    munc = model["label"]
-    time = np.linspace(0, len(infected_by_munc), len(infected_by_munc))
-
-    return initial_v, infected_by_munc, munc, time
+    return {"S": S0, "I": I0, "R": R0}
 
 
 def build_json_params(models_json, infected, params_to_estimate):
     output = []
     for model in models_json:
-        initial_v, infected_by_munc, munc, time = set_initial_values(
-            model, infected)
+        initial_v = get_initial_values_SIR(model)
+        infected_by_munc = infected[model["label"]][START_INFECTED:]
+        munc = model["label"]
+        time = np.linspace(0,len(infected_by_munc),len(infected_by_munc))
 
         new_params = estimate_params(
             infected_by_munc, time, params_to_estimate, initial_v, munc)
@@ -196,6 +185,7 @@ def build_json_params(models_json, infected, params_to_estimate):
     return output
 
 
-def get_params_estimation(nodes_params_json_path, infected, params_to_estiamte):
-    models = read_json(nodes_params_json_path)
+def get_params_estimation(params_json_path, infected, params_to_estiamte):
+    models = read_json(params_json_path)
     return build_json_params(models, infected, params_to_estiamte)
+
