ceeee4e065b2678ca66f7e0e42a7543ac5faae56
Abel <abelo08cruz@gmail.com>
2022-10-18 13:18:54 -0400

    brings numba

--- src/mmodel/simple_trip.py
+++ src/mmodel/simple_trip.py
@@ -86,7 +86,9 @@ class SimpleTripMetaModel(MetaModel):
         def get_global_symbol(k, j):
             return "(" + "+".join([f"y[{local_pos(k,i,j)}]" for i in range(N)]) + ")"
 
-        code = "from scipy.integrate import odeint\n\n\n"
+        code = "from numba import jit\n"
+        code += "from scipy.integrate import odeint\n\n\n"
+        code += "@jit(target_backend='cuda')\n"
         code += "def deriv(y, t, params):\n"
         code += "\tresult = [0] * len(y)\n"
 
