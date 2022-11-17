63ee72dfdca8d1d58323f981d29949c2a644e23b
Abel <abelo08cruz@gmail.com>
2022-11-17 14:18:43 -0500

    gets correctly values of havana when using classic SIR

--- src/mmodel/params_estimation/classic_models.py
+++ src/mmodel/params_estimation/classic_models.py
@@ -5,41 +5,56 @@ from .estimator_class_base import estimator_base
 
 
 class estimator_SIR_classic(estimator_base):
-    def get_initial_values_metamodel(self, models, infected_all_comb):
+    def get_initial_values_metamodel(self, models, infected):
         initial_v, guess = self.api.import_params(self.params_path)
+        guess = guess[:2]
         # its where the value of S is in the list for each muncp(order:S,I,R,N)
         time = -np.inf
         index_N = len(models[0]['model'])
         index_S = models[0]['model'].index('S')
+        total_popultaion = 0
+        total_susceptibles = 0
+        total_infected = 0
 
-        initial_v[index_S] = initial_v[index_S + index_N] - \
-            infected_all_comb[START_INFECTED]
+        for model in models:
+            total_infected += infected[model["label"]][START_INFECTED]
 
-        index_I = index_S+1
+            infected_by_munc = infected[model["label"]][START_INFECTED:]
+            if (len(infected_by_munc) > time):
+                time = len(infected_by_munc)
 
-        initial_v[index_I] = infected_all_comb[START_INFECTED]
+            total_popultaion += initial_v[index_S + index_N]
+            total_susceptibles += initial_v[index_S +
+                                            index_N] - infected_by_munc[0]
 
-        return initial_v, guess, np.linspace(0, time, time)
+            # where the initial values of other munc start
+            index_S += len(model["y"])
+
+        return [total_susceptibles, total_infected, 0, total_popultaion], guess, np.linspace(0, time, time)
 
     def build_json_params_metamodel(self, models_json, acc_infected_by_munc, d_op):
         output = []
         acc_infected_combine = d_op.combine_infected_all_mcps(
-            acc_infected_by_munc)
+            acc_infected_by_munc)[START_INFECTED:]
 
         initial_v, guess, time = self.get_initial_values_metamodel(
-            models_json, acc_infected_combine)
+            models_json, acc_infected_by_munc)
 
         muncps = [model["label"] for model in models_json]
 
-        params_names = list(models_json[0]["params"].keys())
-        acc_infected_combine = acc_infected_combine[START_INFECTED:]
+        params_names = list(models_json[0]["params"].keys())[:2]
 
         new_params, time = self.opt_func.estimate_params_metamodel(
             acc_infected_combine, time, muncps, initial_v, guess, params_names)
 
-        for i, model in enumerate(models_json):
-            model["params"] = new_params[i]
-            output.append(model)
+        models_json[0]["params"] = new_params.pop()
+        models_json[0]["label"] = "Habana"
+        models_json[0]["y"]["S"] = initial_v[0]
+        models_json[0]["y"]["I"] = initial_v[1]
+        models_json[0]["y"]["R"] = initial_v[2]
+        models_json[0]["y"]["N"] = initial_v[3]
+
+        output.append(models_json[0])
 
         return output, time
 
---
+++ tests/mmodel/havana_all_Conn_Classic_SIR/model_havana_d29.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.integrate import odeint
+from numba import njit, jit, cuda
+
+
+def deriv(y, t, params):
+	result = np.zeros(shape = (y.size,), dtype=np.float64)
+	result[0] =  -(params[0] * y[0] * (y[1])) / ((y[3])) + 1 * y[0] - 1 * y[0]
+	result[1] =  (params[0] * y[0] * (y[1])) / ((y[3])) - params[1] * y[1] + 1 * y[1] - 1 * y[1]
+	result[2] =  params[1] * y[1] + 1 * y[2] - 1 * y[2]
+	result[3] =  0 + 1 * y[3] - 1 * y[3]
+	return result
+
+
+def solve(y, t, params):
+	return odeint(deriv, y, t, args=(params,))
